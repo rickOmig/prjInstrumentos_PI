06a6e2d318daa9e9d5c581dfce2bdf722b400857
matheus henrique <matheushenriq.costa@gmail.com>
2023-04-12 14:24:32 -0300

    adicionar novos arquivos do projeto

---
+++ prjInstrumentos_PI-master/src/main/java/lojainstrumentos/lojainstrumentos/LojaInstrumentos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lojainstrumentos.lojainstrumentos;
+
+/**
+ *
+ * @author 55119
+ */
+public class LojaInstrumentos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
