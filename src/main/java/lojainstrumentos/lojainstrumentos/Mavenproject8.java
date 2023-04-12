1008e489d2b03b62dcca46bd028e1b3055279168
Manda-Amanda <alvesdasilvaamanda021@gmail.com>
2023-04-11 21:43:56 -0300

    adicionar projeto

---
+++ src/main/java/lojainstrumentos/lojainstrumentos/Mavenproject8.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lojainstrumentos.lojainstrumentos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alves
+ */
+public class Mavenproject8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String campo_telefoneKey = "a8946239";
+        char[] charCampoTelefone = campo_telefoneKey.toCharArray();
+        for (char character : charCampoTelefone) {
+            //char c = character;// evt.getKeyChar();
+        
+            // check if ch is a digit
+            if (!(character >= '0' && character <= '9')) {
+            //if((c<'0'|| c>'11') &&(c!=keyEvent.VK_BACK_SPACE)){
+                //evt.consume();
+                //JOptionPane.showMessageDialog(rootPane,"Somente nùmeros no campo telefone!");
+                System.err.print("Somente nùmeros no campo telefone!");
+            }
+        }
+    }
+}
